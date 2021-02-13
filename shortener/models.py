f6d0a146de87d2560141027478f1a5ff1a848024
pulga <akindeleodedoyin@gmail.com>
2021-02-13 12:16:14 +0100

    tweaked the shortener model and view a bit to allow shortened urls by anonymous(logged out) users

--- shortener/models.py
+++ shortener/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class Url(models.Model):
     url = models.CharField(max_length=1000)
     short_form = models.CharField(max_length=10)
-    creator = models.ForeignKey(get_user_model(), blank=True, on_delete=models.CASCADE)
+    creator = models.ForeignKey(get_user_model(), blank=True, default=None, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.url} created by {self.creator}'
--- shortener/views.py
+++ shortener/views.py
@@ -2,6 +2,7 @@ import uuid
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.models import AnonymousUser
 
 from .models import Url
 
@@ -17,7 +18,10 @@ def saveUrl(request):
     if request.method == 'POST':
         url = request.POST.get('url')
         short_form = str(uuid.uuid4())[:5]
-        new_url = Url(url=url, short_form=short_form, creator=request.user)
+        new_url = Url(url=url,
+         short_form=short_form,
+         creator=None if isinstance(request.user, AnonymousUser) else request.user)
+        print(request.user)
         new_url.save()
         return render(request, 'home.html', {'short_form': short_form})
 
