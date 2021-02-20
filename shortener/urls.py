728b2a165eb76e132ea8731ed681cb92b3f85ab0
pulga <akindeleodedoyin@gmail.com>
2021-02-20 06:19:54 +0100

    removed 'visit' in the 'visit' url in shortener/urls.py

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,7 +4,7 @@ from .views import HomePageView, CreateUrlView, MyUrlView, saveUrl, visit
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('visit/<str:pk>', visit, name='visit'),
+    path('<str:pk>', visit, name='visit'),
     path('shorten-url/', CreateUrlView.as_view(), name='create_url'),
     path('save-url/', saveUrl, name='save_url'),
     path('my-urls/', MyUrlView.as_view(), name='my_urls'),
